ef08739505f02207cd8a6a7e95ae22884aa4d599
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-01-22 20:50:26 +0100

    Added support for all methods, periodic checkers, events...

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function Prices(options) {
     this.apiKey = options.apiKey;
     this.retry = options.retry || true;
     this.retryTime = options.retryTime || 2 * 1000;
-    this.getListTime = options.listTime || 10 * 1000;
     this.getPricesTime = options.pollTime || 30 * 1000;
     this.getCurrenciesTime = options.currenciesTime || 10 * 60 * 1000;
 
@@ -44,12 +43,22 @@ Prices.prototype.init = function(callback) {
             }
 
             callback(null);
-            self.emit('ready');
+            self.startChecker();
         });
     });
+};
+
+Prices.prototype.setPrices = function(prices) {
+    this.prices = prices;
+};
+
+Prices.prototype.startChecker = function() {
+    this._priceChecker = setInterval(Prices.prototype.getPrices.bind(this), this.getPricesTime);
+    this._currenciesChecker = setInterval(Prices.prototype.getCurrencies.bind(this), this.getCurrenciesTime);
 };
 
 require('./lib/http.js');
 require('./lib/webapi.js');
 require('./lib/requests.js');
 require('./lib/methods.js');
+require('./lib/prices.js');
